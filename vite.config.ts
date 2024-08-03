0cbccc09cd1abc9d731729dfef20e202d5a2aa83
Arcadiano <100984293+ArcadiaAlejandro@users.noreply.github.com>
2024-08-03 07:27:49 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://www.inversionesmcgiver.com/"
+  base: "https://arcadiaalejandro.github.io/inversionesmc/"
     })
