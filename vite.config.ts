e1e00ed0474b322035abb691e1f134ab9ee622fe
Alejandro Antonio <alejandrodelacruzcharun@gmail.com>
2024-08-02 22:55:15 -0500

    se ha arreglado el archivo main para el routeo

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://ArcadiaAlejandro.github.io/inversionesmc/',
+  base: "https://arcadiaalejandro.github.io/inversionesmc/",
 })
